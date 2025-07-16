3416dc6ed86abe6a54cf06abe844775ff62c77fd
enriquehernandezi <ehernandez20@luc.edu>
2025-07-16 02:56:35 +0000

    week 07 1

--- SuperAles.py
+++ SuperAles.py
@@ -1,4 +1,33 @@
+class Ale:
+    def __init__(self, name, abv):
+        self.name = name
+        self.abv = abv
 
+    def alcohol_content(self, volume_in_oz):
+        return self.abv * volume_in_oz
+
+    def description(self):
+        return f"{self.name}: {self.abv * 100:.1f}% ABV"
+
+
+class Blonde(Ale):
+    def __init__(self):
+        Ale.__init__(self, " Blonde ", 0.05)
+
+
+class IPA(Ale):
+    def __init__(self):
+        Ale.__init__(self, "IPA", 0.06)
+
+
+class Wheat(Ale):
+    def __init__(self):
+        Ale.__init__(self, "Wheat", 0.07)
+
+
+class ScotchAle(Ale):
+    def __init__(self):
+        Ale.__init__(self, "Scotch Ale", 0.08)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
--- SuperVehicles.py
+++ SuperVehicles.py
@@ -1,4 +1,38 @@
+class Vehicle:
+    def __init__(self, name, mpg):
+        self.name = name
+        self.mpg = mpg
 
+    def fuel_needed(self, distance):
+        return distance / self.mpg
+
+    def description(self):
+        return f"{self.name} gets {self.mpg} miles per gallon."
+
+
+class Car(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self, "Car", 10)
+
+
+class Bike(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self, "Bike", 20)
+
+
+class Motorcycle(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self, "Motorcycle", 30)
+
+
+class Train(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self, "Train", 40)
+
+#Reflection
+#1 Didn’t add comments, which hurts readability for others. Variable naming is good, but structure could use a bit more documentation.
+#2 Didn’t check if the list was empty. Manually tracked count even though len() is simpler.
+#3 Didn’t finish the function, missing the _ case, no check for non-alpha words, and didn’t print the final result.
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
